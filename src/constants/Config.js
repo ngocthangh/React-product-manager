fd17f95bef951a26df06ef6309ad64fe22d0c341
NgocThang <ngocthanguit@gmail.com>
2019-04-10 16:43:26 +0700

    Form validate

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,5 +1,6 @@
 export const Config = {
-    ROOT_URL : 'https://5ca173ccc1b53400149aca50.mockapi.io/api',
+    ROOT_URL0 : 'https://5ca173ccc1b53400149aca50.mockapi.io/api',
+    ROOT_URL : 'http://localhost:3002',
     Endpoint: {
         ListProduct: 'products'
     }
--- src/pages/ProductActionPage/ProductActionPage.js
+++ src/pages/ProductActionPage/ProductActionPage.js
@@ -3,10 +3,53 @@ import './ProductActionPage.css';
 import * as Actions from '../../actions/index';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import FormValidator from './FormValidator';
 
 class ProductActionPage extends Component {
     constructor(props){
         super(props);
+        this.validator = new FormValidator([
+            { 
+              field: 'name', 
+              method: 'isEmpty', 
+              validWhen: false, 
+              message: 'Name is required.' 
+            },
+            { 
+              field: 'price', 
+              method: 'isEmpty', 
+              validWhen: false, 
+              message: 'Pleave provide price.'
+            },
+            { 
+              field: 'image', 
+              method: 'isEmpty', 
+              validWhen: false, 
+              message: 'Image is required.'
+            },
+            { 
+              field: 'stock', 
+              method: 'isEmpty', 
+              validWhen: false, 
+              message: 'Stock is required.'
+            },
+            { 
+              field: 'price', 
+              method: 'isInt', 
+              args: [{min: 1}],
+              validWhen: true, 
+              message: 'Price must be positive number.'
+            },
+            { 
+              field: 'stock', 
+              method: 'isInt', 
+              args: [{min: 1}],
+              validWhen: true, 
+              message: 'Stock must be positive number.'
+            }
+          ]);
+      
+          this.submitted = false;
         this.state = {
             id: '',
             name: '',
@@ -14,7 +57,9 @@ class ProductActionPage extends Component {
             price: '',
             stock: '',
             kind: '',
-            display: true};
+            display: true,
+            validation: this.validator.valid()
+        };
     }
     componentDidMount(){
         let { location } = this.props;
@@ -41,8 +86,22 @@ class ProductActionPage extends Component {
         let { history } = this.props;
         history.push('/');
     }
+    handleFormSubmit = event => {
+        event.preventDefault();
+    
+        const validation = this.validator.validate(this.state);
+        this.setState({ validation });
+        this.submitted = true;
+    
+        if (validation.isValid) {
+          this.saveProduct();
+        }
+      }
     render() {
         let { name, image, price, stock, display } = this.state;
+        let validation = this.submitted ?                         // if the form has been submitted at least once
+                      this.validator.validate(this.state) :   // then check validity every time we render
+                      this.state.validation                   // otherwise just use what's in state
         return (
             <div className="row">
                 <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
@@ -51,18 +110,22 @@ class ProductActionPage extends Component {
                         <div className="form-group">
                             <label htmlFor="name">Sản Phẩm</label>
                             <input type="text" className="form-control" name="name" value={name} onChange={this.onChange}/>
+                            <span className="help-block">{validation.name.message}</span>
                         </div>
                         <div className="form-group">
                             <label htmlFor="image">Hình Ảnh</label>
                             <input type="text" className="form-control" name="image" value={image} onChange={this.onChange}/>
+                            <span className="help-block">{validation.image.message}</span>
                         </div>
                         <div className="form-group">
                             <label htmlFor="price">Giá</label>
                             <input type="text" className="form-control" name="price" value={price} onChange={this.onChange}/>
+                            <span className="help-block">{validation.price.message}</span>
                         </div>
                         <div className="form-group">
                             <label htmlFor="stock">Số Lượng Tồn</label>
                             <input type="text" className="form-control" name="stock" value={stock} onChange={this.onChange}/>
+                            <span className="help-block">{validation.stock.message}</span>
                         </div>
                         {/* <div className="form-group">
                             <label htmlFor="kind">Loại Sản Phẩm</label>
@@ -78,7 +141,7 @@ class ProductActionPage extends Component {
                         </div>
 
                         <div className="submit-button">
-                            <button type='button' className="btn btn-primary form-action" onClick={this.saveProduct}>Lưu</button>
+                            <button type='button' className="btn btn-primary form-action" onClick={this.handleFormSubmit}>Lưu</button>
                             <Link to='/' className="btn btn-warning form-action">Hủy</Link>
                         </div>
                     </form>
