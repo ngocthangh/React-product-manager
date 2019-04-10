b97142f59e4a67772dccd102f4b0d92d30aae86f
NgocThang <ngocthanguit@gmail.com>
2019-04-10 22:38:00 +0700

    add docs - build

---
+++ docs/precache-manifest.b26f5b0ba6a7936cfebbc3f1db90d8b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "af3a311933887ab0db8d",
+    "url": "/static/js/main.e4b6e4ac.chunk.js"
+  },
+  {
+    "revision": "7ab8bb8be89cb19a3ec8",
+    "url": "/static/js/2.289dfbfe.chunk.js"
+  },
+  {
+    "revision": "af3a311933887ab0db8d",
+    "url": "/static/css/main.958b338c.chunk.css"
+  },
+  {
+    "revision": "9fcf5093aa763caf1e244c505a81add7",
+    "url": "/index.html"
+  }
+];
